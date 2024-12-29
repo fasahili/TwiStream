ee658bf8eb0e26b4e632e0abe80bda9d8f7d7ebf
Ahmad Irshaid <145376048+AhmadIrsahaid@users.noreply.github.com>
2024-12-29 15:03:37 +0200

    implement Producer

--- src/main/scala/kafka/Producer.scala
+++ src/main/scala/kafka/Producer.scala
@@ -9,35 +9,30 @@ import scala.io.Source
 object Producer {
   def main(args: Array[String]): Unit = {
     val topic = "try"
+    val properties = new Properties()
+    properties.put("bootstrap.servers", "localhost:9092")
+    properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-    // إعداد خصائص الاتصال بكافكا
-    val props = new Properties()
-    props.put("bootstrap.servers", "localhost:9092")
-    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-    // إنشاء منتج Kafka
-    val producer = new KafkaProducer[String, String](props)
+    val producer = new KafkaProducer[String, String](properties)
     val filePath = "src/main/scala/data/boulder_flood_geolocated_tweets.json"
 
-    // قراءة الملف وإرسال كل تغريدة بعد معالجتها
     val source = Source.fromFile(filePath)
     try {
       source.getLines()
         .map { line =>
-          // تحويل كل سطر إلى JSON ومعالجة البيانات
           val record = Json.parse(line)
           val createdAt = (record \ "created_at").asOpt[String].getOrElse("N/A")
           val text = (record \ "text").asOpt[String].getOrElse("N/A")
           val hashtags = (record \ "entities" \ "hashtags").asOpt[Seq[JsValue]].getOrElse(Seq())
             .map(tag => (tag \ "text").asOpt[String].getOrElse(""))
-
           val location = (record \ "user" \ "location").asOpt[String].getOrElse("Unknown")
           val geoCoordinates = (record \ "geo" \ "coordinates").asOpt[Seq[Double]]
 
-          // تكوين البيانات التي سيتم إرسالها بناءً على وجود geoCoordinates
+
           val filteredData = {
-            val baseData = Json.obj(
+            val kafkaValues = Json.obj(
               "created_at" -> createdAt,
               "text" -> text,
               "hashtags" -> hashtags,
@@ -45,15 +40,15 @@ object Producer {
             )
             geoCoordinates match {
               case Some(Seq(lat, lon)) =>
-                baseData ++ Json.obj("geo" -> Json.arr(lon, lat))
-              case None => baseData
+                kafkaValues ++ Json.obj("geo" -> Json.arr(lon, lat))
+              case None => kafkaValues
             }
           }
 
           filteredData
         }
         .foreach { filteredData =>
-          // إرسال التغريدة المفلترة إلى Kafka
+
           producer.send(new ProducerRecord[String, String](topic, null, filteredData.toString()))
           println(s"Sent: $filteredData")
         }
