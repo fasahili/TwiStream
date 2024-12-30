d3d06639750fcfe748346d23c98742fcc1e6f636
Ahmad Irshaid <145376048+AhmadIrsahaid@users.noreply.github.com>
2024-12-30 16:47:19 +0200

    add time to sent tweets to consumer

--- src/main/scala/kafka/Producer.scala
+++ src/main/scala/kafka/Producer.scala
@@ -5,6 +5,7 @@ import play.api.libs.json._
 
 import java.util.Properties
 import scala.io.Source
+import scala.concurrent.duration._
 
 object Producer {
   def main(args: Array[String]): Unit = {
@@ -14,47 +15,48 @@ object Producer {
     properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-
     val producer = new KafkaProducer[String, String](properties)
     val filePath = "src/main/scala/data/boulder_flood_geolocated_tweets.json"
 
     val source = Source.fromFile(filePath)
     try {
-      source.getLines()
-        .map { line =>
-          val record = Json.parse(line)
-          val createdAt = (record \ "created_at").asOpt[String].getOrElse("N/A")
-          val text = (record \ "text").asOpt[String].getOrElse("N/A")
-          val hashtags = (record \ "entities" \ "hashtags").asOpt[Seq[JsValue]].getOrElse(Seq())
-            .map(tag => (tag \ "text").asOpt[String].getOrElse(""))
-          val location = (record \ "user" \ "location").asOpt[String].getOrElse("Unknown")
-          val geoCoordinates = (record \ "geo" \ "coordinates").asOpt[Seq[Double]]
+      val records = source.getLines().map { line =>
+        val record = Json.parse(line)
+        val createdAt = (record \ "created_at").headOption.flatMap(_.asOpt[String]).getOrElse("N/A")
+        val text = (record \ "text").headOption.flatMap(_.asOpt[String]).getOrElse("N/A")
+        val hashtags = (record \ "entities" \ "hashtags").headOption.flatMap(_.asOpt[Seq[JsValue]]).getOrElse(Seq())
+          .map(tag => (tag \ "text").headOption.flatMap(_.asOpt[String]).getOrElse(""))
+        val location = (record \ "user" \ "location").headOption.flatMap(_.asOpt[String]).getOrElse("Unknown")
+        val geoCoordinates = (record \ "geo" \ "coordinates").headOption.flatMap(_.asOpt[Seq[Double]])
 
-
-          val filteredData = {
-            val kafkaValues = Json.obj(
-              "created_at" -> createdAt,
-              "text" -> text,
-              "hashtags" -> hashtags,
-              "location" -> location
-            )
-            geoCoordinates match {
-              case Some(Seq(lat, lon)) =>
-                kafkaValues ++ Json.obj("geo" -> Json.arr(lon, lat))
-              case None => kafkaValues
-            }
+        val filteredData = {
+          val kafkaValues = Json.obj(
+            "created_at" -> createdAt,
+            "text" -> text,
+            "hashtags" -> hashtags,
+            "location" -> location
+          )
+          geoCoordinates match {
+            case Some(Seq(lat, lon)) =>
+              kafkaValues ++ Json.obj("geo" -> Json.arr(lon, lat))
+            case None => kafkaValues
           }
-
-          filteredData
         }
-        .foreach { filteredData =>
 
-          producer.send(new ProducerRecord[String, String](topic, null, filteredData.toString()))
+        filteredData.toString()
+      }.toSeq
+
+      records.grouped(1000).foreach { batch =>
+        batch.foreach { filteredData =>
+          producer.send(new ProducerRecord[String, String](topic, null, filteredData))
           println(s"Sent: $filteredData")
         }
+        Thread.sleep(1000)
+      }
+
     } catch {
       case ex: Exception =>
-        println(s"Error opening file: $filePath. Error: ${ex.getMessage}")
+        println(s"Error processing file: $filePath. Error: ${ex.getMessage}")
     } finally {
       source.close()
       producer.close()
